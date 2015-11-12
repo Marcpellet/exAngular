66cd1bdd3f5a16a4bfd34c9d9a4663aac610a7bd
Marc Pellet <marc.pellet@heig-vd.ch>
2015-11-13 00:24:57 +0100

    tentative d'ajout d'un graph

--- public/js/app/demo-app.js
+++ public/js/app/demo-app.js
@@ -1,7 +1,12 @@
 (function(){
-  var appModule = angular.module("test_module", []);
+  var appModule = angular.module("test_module", ["chart.js"]);
 
   appModule.controller("test_controller", function($scope){
     $scope.title = "Test Angular";
+  });
+
+  .controller("DoughnutCtrl",	function	($scope)	{
+		$scope.labels	=	["Download	Sales",	"In-Store	Sales",	"Mail-Order	Sales"];
+		$scope.data	=	[300,	500,	100];
   });
 })();
